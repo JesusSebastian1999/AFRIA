89fe979799b06356ae46f9eca2ade470a6c7cdf0
JesusSebastian1999 <SebasUTN12@hotmail.com>
2019-11-24 18:42:18 -0600

    .

--- public/js/mi-navegacion.js
+++ public/js/mi-navegacion.js
@@ -7,18 +7,24 @@ customElements.define("mi-navegacion", class extends HTMLElement {
     this.innerHTML = /* html */ `<progress max="100">Cargando…</progress>`;
     auth.onAuthStateChanged(usuarioAuth =>
       catchas(async () => {
-        this.innerHTML = /* html */ `<a href="index.html">Inicio</a>`;
+        this.innerHTML = /* html */ `<li class="nav-item">
+        <a class="nav-link" href="index.html"><i class="fas fa-user"></i>Inicio</a>
+       </li>`;
         if (usuarioAuth && usuarioAuth.email) {
           // Usuario aceptado.
           const roles =
             Object.freeze(new Set((await verifica(usuarioAuth)).roles));
           if (roles.has("Administrador")) {
             this.innerHTML += /* html */
-              `<a href="../html/administrador/index.html">Administradores</a>`;
+              `<li class="nav-item">
+              <a class="nav-link" href="html/administrador/index.html"><i class="fas fa-user"></i>Administrador</a>
+             </li>`;
           }
           if (roles.has("Profesore")) {
             this.innerHTML += /* html */
-              `<a href="../html/profesor/index.html">Profesores</a>`;
+              `<li class="nav-item">
+              <a class="nav-link" href="html/profesor/index.html"><i class="fas fa-user"></i>Profesor</a>
+             </li>`;
           }
           this.innerHTML += /*html*/ `<a href="FormSesion.html">Sesión</a>`;
           this.innerHTML += /*html*/
